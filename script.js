13d929783e20faf656769cc77eda5612f1578ca4
adyismad <adyismad@bu.edu>
2021-11-29 13:13:45 -0500

    styling and validation

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
+
+//Main method
 function validateForm() {
-  let firstName = document.forms["myForm"]["firstName"].value;
-  let lastName = document.forms["myForm"]["lastName"].value;
   let facilitator = document.forms["myForm"]["facilitator"].value;
 
-  if (!isValidName(firstName)) {
-    alert("Please correct the first name");
-    return false;
-  }
-  if (!isValidName(lastName)) {
-    alert("Please correct the last name");
-    return false;
-  }
-
-  if(!validateFacilitator(facilitator)){
+    //check to see if entered facilitator name is valid or not, if not valid show an alert message
+  if (!validateFacilitator(facilitator)) {
     alert("Please enter valid facilitator name");
     return false;
   }
 }
 
-function isValidName(name) {
-  return /^([A-Za-z]{2,})$/.test(name);
-}
-
+//method to validate whether entered facilitator name matches in the array
 function validateFacilitator(facilitator) {
   const facilitators = ["Chris", "Josh", "Fazil", "Christian"];
-  if (facilitators.includes(facilitator)) {
-    return true;
-  }
-  return false;
+  const includesValue = facilitators.some((element) => {
+    return element.toLowerCase() === facilitator.toLowerCase();
+  });
+  return includesValue;
 }
